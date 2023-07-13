7ddd099e040dd55f7889d0bd34062f18a41fddc8
Amir 0d <amirmsut@gmail.com>
2023-07-13 14:58:02 +0330

    add coin component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { getCoin } from "../services/api";
 import Loader from "./Loader";
+import Coin from "./Coin";
 
 const Landing = () => {
     const [coins, setCoins] = useState([]);
@@ -19,7 +20,15 @@ const Landing = () => {
             {coins.length ? (
                 <div>
                     {coins.map((item) => (
-                        <p key={item.id}>{item.name}</p>
+                        <Coin
+                            key={item.id}
+                            name={item.name}
+                            image={item.image}
+                            symbol={item.symbol}
+                            price={item.current_price}
+                            marketCap={item.market_cap}
+                            priceChange={item.price_change_percentage_24h}
+                        />
                     ))}
                 </div>
             ) : (
